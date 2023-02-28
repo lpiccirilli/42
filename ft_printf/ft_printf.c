60590ed801886d2542220b4402ce659231505a46
lpiccirilli <119267142+lpiccirilli@users.noreply.github.com>
2023-02-28 19:55:59 +0100

    Add files via upload

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,66 +6,88 @@
 /*   By: lpicciri <lpicciri@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 10:40:05 by lpicciri          #+#    #+#             */
-/*   Updated: 2023/02/03 09:01:00 by lpicciri         ###   ########.fr       */
+/*   Updated: 2023/02/20 20:20:06 by lpicciri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_control(char *str, va_list args, size_t i)
+int	ft_putnbr(long int nbr)
+{
+	long long int	n;
+	int				len;
+
+	n = nbr;
+	len = 0;
+	if (n < 0)
+	{
+		n *= -1;
+		len += ft_putchar('-');
+	}
+	if (n >= 10)
+	{
+		len += ft_putnbr(n / 10);
+	}
+	len += ft_putchar((n % 10) + 48);
+	return (len);
+}
+
+int	ft_printnbr(int nbr)
+{
+	int	len;
+
+	len = 0;
+	if (nbr == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	len += ft_putnbr(nbr);
+	return (len);
+}
+
+int	ft_control(va_list *args, char format)
 {
 	int	counter;
 
 	counter = 0;
-	if (str[i] == 'c')
-		counter += ft_putchar(va_arg(args, int));
-	if (str[i] == 'd' || str[i] == 'i')
-		counter += ft_putnbr(va_arg(args, long long int));
-	else if (str[i] == 'u')
-		counter += ft_putnbr_base(va_arg(args, unsigned int), "0123456789");
-	else if (str[i] == 's')
-		counter += ft_putstr(va_arg(args, char *));
-	else if (str[i] == 'x')
-		counter
-			+= ft_putnbr_base(va_arg(args, unsigned int), "0123456789abcdef");
-	else if (str[i] == 'X')
-		counter
-			+= ft_putnbr_base(va_arg(args, unsigned int), "0123456789ABCDEF");
-	else if (str[i] == '%')
+	if (format == 'c')
+		counter += ft_putchar(va_arg(*args, long long int));
+	else if (format == 'u')
+		counter += ft_printunsigned(va_arg(*args, int));
+	else if (format == 'd' || format == 'i')
+		counter += ft_printnbr(va_arg(*args, int));
+	else if (format == 's')
+		counter += ft_putstr(va_arg(*args, char *));
+	else if (format == 'x' || format == 'X')
+		counter += ft_printhex(va_arg(*args, unsigned int), format);
+	else if (format == '%')
 		counter += ft_putchar('%');
-	else if (str[i] == 'p')
-	{
-		counter += 2;
-		counter += ft_convertion_ptr(va_arg(args, void *));
-	}
+	else if (format == 'p')
+		counter += ft_print_ptr(va_arg(*args, unsigned long long));
 	return (counter);
 }
 
 int	ft_printf(const char *str, ...)
 {
-	size_t	i;
 	va_list	args;
+	int		i;
 	int		printed;
 
-	i = 0;
 	printed = 0;
-	va_start(args, str);
+	i = 0;
+	va_start (args, str);
 	while (str[i] != '\0')
 	{
-		if (str[i] != 37)
+		if (str[i] == '%')
 		{
-			printed += ft_putchar(str[i]);
+			printed += ft_control(&args, str[i + 1]);
 			i++;
 		}
-		else if (str[i] == 37)
-		{
-			if (str[i + 1] == '#' || str[i + 1] == '-' || str[i + 1] == '0'
-				|| str[i + 1] == '+' || str[i + 1] == ' ' || str[i + 1] == '.')
-				printed += ft_bonus((char *)str, ++i);
-			printed += ft_control((char *)str, args, i + 1);
-			i += 2;
-		}
+		else if (str[i] != '%')
+			printed += ft_putchar(str[i]);
+		i++;
 	}
-	va_end(args);
+	va_end (args);
 	return (printed);
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,31 +6,29 @@
 /*   By: lpicciri <lpicciri@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 14:50:52 by lpicciri          #+#    #+#             */
-/*   Updated: 2023/02/02 15:59:10 by lpicciri         ###   ########.fr       */
+/*   Updated: 2023/02/20 20:25:49 by lpicciri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
 # include <stdio.h>
 
-int		ft_control(char *str, va_list args, size_t i);
 int		ft_printf(const char *str, ...);
 int		ft_putchar(int c);
 int		ft_putstr(char *s);
-int		ft_putnbr(long long nb);
-int		ft_putnbr_base(long long n, char *base);
-int		check_base(char *base);
-int		ft_convertion_ptr(void *p);
-int		ft_countnbr(long long nb, char *base);
+void	ft_put_ptr(unsigned long long p);
+int		ft_ptrlen(unsigned long long p);
 int		ft_strlen(const char *s);
-int		ft_bonus(char *str, size_t i);
-int		ft_hashtag_flag(char *str, size_t i);
-int		ft_space_flag(char *str, size_t i);
-int		ft_bonus(char *str, size_t i);
-int		ft_hashtag_flag(char *str, size_t i);
-int		ft_space_flag(char *str, size_t i);
+int		ft_printhex(unsigned int hex, const char format);
+void	ft_putnbrhex(unsigned int hex, const char format);
+int		ft_lenhex(unsigned int nbr);
+int		ft_print_ptr(unsigned long long p);
+int		ft_printunsigned(unsigned int unsnbr);
+int		ft_printnbr(int nbr);
+int		ft_putnbr(long int nbr);
 #endif
